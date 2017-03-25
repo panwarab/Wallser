29d8d30a01718c45c1c05535464b901a08c8953d
Abhiroj Panwar <abhiroj95@gmail.com>
2017-03-26 01:32:14 +0530

    removing useless code

--- app/src/main/java/thenextvoyager/wallser/utility/Utility.java
+++ app/src/main/java/thenextvoyager/wallser/utility/Utility.java
@@ -1,27 +1,19 @@
 package thenextvoyager.wallser.utility;
 
-import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Environment;
 import android.webkit.MimeTypeMap;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 import thenextvoyager.wallser.Data.DataModel;
 import thenextvoyager.wallser.Data.ImageContract;
@@ -135,82 +127,6 @@ public class Utility {
         }
         return false;
     }
-
-    public static class AsyncImageDownloader extends AsyncTask<Object, Object, Void> {
-
-
-        private static final String TAG = AsyncImageDownloader.class.getSimpleName();
-        Context context;
-        String downloadURL;
-        ProgressDialog dialog;
-
-
-        AsyncImageDownloader(Context context, String downloadURL) {
-            this.context = context;
-            this.downloadURL = downloadURL;
-        }
-
-
-        @Override
-        protected Void doInBackground(Object... voids) {
-            try {
-                URL url = new URL("https://unsplash.com/photos/65sru5g6xHk/download");
-                HttpURLConnection connection = getFinalURL(url);
-                connection.setDoInput(true);
-                connection.connect();
-                InputStream inptStream = connection.getInputStream();
-                Bitmap bitmap = BitmapFactory.decodeStream(inptStream);
-                saveImage(getResizedBitmap(bitmap, 25, 25), context, "id of downloaded image",false);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-
-        HttpURLConnection getFinalURL(URL url) throws IOException {
-            URL finalUrl = null;
-            HttpURLConnection.setFollowRedirects(false);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.connect();
-            if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_MOVED_TEMP || httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_MOVED_PERM) {
-                URL redirectUrl = new URL(httpURLConnection.getHeaderField("Location"));
-                finalUrl = redirectUrl;
-            }
-            return (HttpURLConnection) finalUrl.openConnection();
-        }
-
-        public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
-            int width = bm.getWidth();
-            int height = bm.getHeight();
-            float scaleWidth = ((float) newWidth) / width;
-            float scaleHeight = ((float) newHeight) / height;
-            // CREATE A MATRIX FOR THE MANIPULATION
-            Matrix matrix = new Matrix();
-            // RESIZE THE BIT MAP
-            matrix.postScale(scaleWidth, scaleHeight);
-
-            // "RECREATE" THE NEW BITMAP
-            Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height,
-                    matrix, false);
-
-            return resizedBitmap;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            dialog = new ProgressDialog(context);
-            dialog.show();
-            dialog.setMessage("Downloading");
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            dialog.dismiss();
-        }
-    }
-
+    
 
 }
