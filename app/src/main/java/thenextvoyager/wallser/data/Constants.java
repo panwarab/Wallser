10d919b1de182244f7a18cd6d3e9a0c78248f562
Abhiroj Panwar <abhiroj96@gmail.com>
2017-04-15 12:05:02 +0530

    Code-Order: Remove unused variables from Constants.java

--- app/src/main/java/thenextvoyager/wallser/data/Constants.java
+++ app/src/main/java/thenextvoyager/wallser/data/Constants.java
@@ -13,21 +13,16 @@ import thenextvoyager.wallser.R;
 
 public class Constants {
 
-    public static final String IMAGE_PASS_KEY = "image_one_pass";
     public static final int CURSOR_LOADER_MANAGER = 1;
     public static final String MODEL_TAG = "1";
     public static final String IMAGE_FRAGMENT_TAG = "2";
     public static final long HANDLER_DELAY_TIME = 1000;
-    public static final String IMAGE_TAG = "IMAGE_PASS";
-    public static final String SEARCH = "search";
     public static final String CHOICE_TAG="ORDER_BY";
     public static final String DATA_TAG = "DATA";
     public static final int PER_PAGE = 10;
     public static final String PAGEFRAG = "PAGEFRAGMENT";
-    public static final String TAG_TO_FRAG = "TagToFrag";
     public static final String PAGE_NO = "pagenumber";
     public static final int PERMISSION_REQUEST_CODE = 1000;
-    private static final String TAG = Constants.class.getSimpleName();
     public static String api_key;
     public static HashMap<String, Fragment> TagToFrag;
     public static String githubURL = "https://github.com/AbhirojPanwar";
